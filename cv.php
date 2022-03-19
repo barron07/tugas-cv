99b56f4bb285572085865b929259d2a1e2b4c92f
barron07 <100698010+barron07@users.noreply.github.com>
2022-03-19 22:19:34 +0700

    Add files via upload

---
+++ cv.php
@@ -0,0 +1,123 @@
+<html>
+    <head>
+        <title>CURRICULUM VITAE</title>
+        <link rel="icon" type="image/x-icon" href="C:\xampp\htdocs\tugas_cv\image\gold.png">
+        </head>
+
+        <body>
+        <h1 align=center>CURICULUM VITAE</h1>
+        <h3 align=center>Barron Mahardhika Al-Fahmi</h3>
+
+        <style>
+            .q {color:brown;}
+            .p {color: plum;}
+        </style>
+        <table style="width: 100%">
+            <tr>
+              <th style="width:25%"><h2>Social Media</h2></th><hr>
+              <th style="width:30%"><h2 align="left">DATA PRIBADI</h2><hr></th>
+              
+            </tr>
+
+            <tr>
+              <th class="p"><a href="https://www.instagram.com/bvdie13/"><img src="image/instagram.png">
+                <a href="https://github.com/barron07/tugas-cv"><img src="image/github.png"></a></th>
+              <th align="left">Nama : Barron Mahardhika Al-Fahmi</th>
+              
+            </tr>
+
+            <tr>
+              <th class="q"></a></th>
+              <th align="left">NIM : 19410100107</th>
+              
+            </tr>
+
+            <tr>
+                <th></th>
+                <th align="left">Tempat, Tanggal Lahir : Kediri, 13 Agustus 2000</th>
+            </tr>
+
+            <tr>
+                <th></th>
+                <th align="left">Jenis Kelamin : Laki-Laki</th>
+            </tr>
+
+            <tr>
+                <th></th>
+                <th align="left">Agama : Islam</th>
+            </tr>
+
+            <tr>
+                <th></th>
+                <th align="left">Alamat : Jl. Semampir Selatan V A, Medokan Semampir, Sukolilo, Surabaya</th>
+            </tr>
+
+            <tr>
+                <th></th>
+                <th align="left">Email : 19410100107@dinamika.ac.id<hr></th>
+            </tr>
+
+            <tr>
+                <th></th>
+                <th style="width:70%"><h2 align="left">DATA PENDIDIKAN</h2><hr></th>
+            </tr>
+    
+            <tr>
+                <th></th>
+                <th align="left">SD : SDN Banjarsari 1</th>
+            </tr>
+
+            <tr>
+                <th></th>
+                <th align="left">SMP : SMPN 1 Bojonegoro</th>
+            </tr>
+
+            <tr>
+                <th></th>
+                <th align="left">SMA : SMAN 1 Bojonegoro</th>
+            </tr>   
+            
+            <tr>
+                <th></th>
+                <th align="left">Universitas : Dinamika Surabaya<hr></th>
+            </tr>
+
+            <tr>
+                <th></th>
+                <th style="width:70%"><h2 align="left">KEAHLIAN</h2><hr></th>
+                </tr>
+
+            <tr>
+                <th></th>
+                <th align="left">Bahasa Pemrograman : HTML, CSS, PHP, MySQL</th>
+            </tr>  
+            
+            <tr>
+                <th></th>
+                <th align="left">Bahasa : Bahasa Indonesia<hr></th>
+            </tr> 
+
+            <tr>
+                <th></th>
+                <th style="width:70%"><h2 align="left">PENGALAMAN</h2><hr></th>
+            </tr>
+
+            <tr>
+                <th></th>
+                <th align="left">1. Staff Kewirausahaan HMSI Universitas Dinamika Surabaya</th>
+            </tr> 
+
+            <tr>
+                <th>-</th>
+                <th align="left">2. Panitia SIGN IN Universitas Dinamika Surabaya</th>
+            </tr> 
+
+          </table>
+
+
+        </body>
+
+
+
+
+</html>
